5e3e7c68e34bba7df19c46a40834909265ea7701
Ananya-vv <ananyaanandan24@gmail.com>
2023-05-16 15:11:49 +0530

    Initial commit

---
+++ lib/image.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class pimage extends StatefulWidget {
+  const image({Key? key}) : super(key: key);
+
+  @override
+  State<image> createState() => _imageState();
+}
+
+class _imageState extends State<image> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ,
+  }
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:project/image.dart';
+void main(){
+  runApp(pimage());
+
+}
